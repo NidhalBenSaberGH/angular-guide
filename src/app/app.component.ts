b44d14143847adba48bb60e4147b4801840da1e4
NIDHAL BEN SABER <nidhal.ben-saber@talan.com>
2021-07-31 12:17:26 +0100

    when we use @Injectable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,6 @@ import {AccountsService} from "./accounts.service";
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: []
 })
 export class AppComponent implements OnInit{
 
