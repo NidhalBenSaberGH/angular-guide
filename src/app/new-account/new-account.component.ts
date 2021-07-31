276364af30b1c87dd5d1413312734f97888d937a
NIDHAL BEN SABER <nidhal.ben-saber@talan.com>
2021-07-31 12:35:21 +0100

    event emitter en service

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -12,6 +12,7 @@ export class NewAccountComponent {
 
   constructor(private loggingService: LoggingService,
               private accountsService: AccountsService) {
+    this.accountsService.statusUpdated.subscribe((status: string) => alert('New Status ' + status));
 
   }
 
