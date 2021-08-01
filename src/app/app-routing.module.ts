268a7389aadada680b8f4740f51ea7266970ce65
NIDHAL BEN SABER <nidhal.ben-saber@talan.com>
2021-08-01 11:35:27 +0100

    queryParams ?properties=value& fragment #

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import {NgModule} from "@angular/core";
 import {RouterModule} from "@angular/router";
 import {HomeComponent} from "./home/home.component";
 import {UserComponent} from "./users/user/user.component";
+import {EditServerComponent} from "./servers/edit-server/edit-server.component";
 
 const appRoutes = [
   {path: '', component: HomeComponent},
   {path: 'users', component: UsersComponent},
   {path: 'users/:id/:name', component: UserComponent},
-  {path: 'servers', component: ServersComponent}
+  {path: 'servers', component: ServersComponent},
+  {path: 'servers/:id/edit', component: EditServerComponent}
 ];
 
 @NgModule({
