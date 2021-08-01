d124abdcaafe7b96f6ba45643a60205e8353f3a5
NIDHAL BEN SABER <nidhal.ben-saber@talan.com>
2021-08-01 11:06:10 +0100

    ActivatedRoute params snaphot

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import {ServersComponent} from "./servers/servers.component";
 import {NgModule} from "@angular/core";
 import {RouterModule} from "@angular/router";
 import {HomeComponent} from "./home/home.component";
+import {UserComponent} from "./users/user/user.component";
 
 const appRoutes = [
   {path: '', component: HomeComponent},
   {path: 'users', component: UsersComponent},
+  {path: 'users/:id/:name', component: UserComponent},
   {path: 'servers', component: ServersComponent}
 ];
 
