e922a360878abc71cb06ff4e96ab89a503f50711
NIDHAL BEN SABER <nidhal.ben-saber@talan.com>
2021-08-01 13:01:38 +0100

    nested route children nested router outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,11 +10,14 @@ import {ServerComponent} from "./servers/server/server.component";
 
 const appRoutes = [
   {path: '', component: HomeComponent},
-  {path: 'users', component: UsersComponent},
-  {path: 'users/:id/:name', component: UserComponent},
-  {path: 'servers', component: ServersComponent},
-  {path: 'servers/:id', component: ServerComponent},
-  {path: 'servers/:id/edit', component: EditServerComponent},
+  {path: 'users', component: UsersComponent, children: [
+      {path: ':id/:name', component: UserComponent},
+    ]},
+  {path: 'servers', component: ServersComponent, children: [
+      {path: ':id', component: ServerComponent},
+      {path: ':id/edit', component: EditServerComponent},
+    ]},
+
   {path: 'not-found', component: PageNotFoundComponent},
   {path: '**', redirectTo: '/not-found'}
 ];
