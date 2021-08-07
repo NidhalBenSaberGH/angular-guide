d4efe90963ae52e861ec8ef4f2396f97a622a911
NIDHAL BEN SABER <nidhal.ben-saber@talan.com>
2021-08-07 16:25:17 +0100

    Td get data from form  reset form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,9 +12,17 @@ export class AppComponent {
   defaultQuestion = 'teacher';
   answer = '';
   genders = ['male', 'female'];
+  submitted = false;
+
+  user = {username: '', email: '', secretQuestion: '', answer: '', gender: ''};
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    this.signupForm.form.patchValue({
+      userData: {
+        username: "Superuser"
+      }
+    });
   }
 
   // onSubmit(form: NgForm) {
@@ -23,6 +31,17 @@ export class AppComponent {
   // }
 
   onSubmit() {
-  console.log(this.signupForm);
+    console.log(this.signupForm);
+
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
+
+
   }
 }
