0ab4ce7b33bf7bdb77f2a4a4eacd87f95c2238b5
NIDHAL BEN SABER <nidhal.ben-saber@talan.com>
2021-08-07 12:47:10 +0100

    init fro forms examples

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,47 +1,19 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import {BasicHighlightDirective} from "./shared/basic-highlight/basic-highlight.directive";
-import { BetterHighlightDirective } from './shared/better-highlight/better-highlight.directive';
-import { UnlessDirective } from './unless.directive';
-import { DropdownDirective } from './shared/dropdown.directive';
-import {ShoppingListService} from "./shopping-list/shopping-list.service";
-import {AppRoutingModule} from "./app-routing.module";
-import {RecipeStartComponent} from "./recipes/recipe-start/recipe-start.component";
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
-    BasicHighlightDirective,
-    BetterHighlightDirective,
-    UnlessDirective,
-    DropdownDirective,
-    RecipeStartComponent,
-    RecipeEditComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule,
-    AppRoutingModule
+    FormsModule,
   ],
-  providers: [ShoppingListService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
