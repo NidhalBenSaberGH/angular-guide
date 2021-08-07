15f9f8008601773e2983d436f55ed00eb3084143
NIDHAL BEN SABER <nidhal.ben-saber@talan.com>
2021-08-07 12:20:15 +0100

    example of using Subject instead of even emitter

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,11 @@
 import {Ingredient} from "../shared/ingredient.model";
-import {EventEmitter} from "@angular/core";
+import {Subject} from "rxjs";
 
 export class ShoppingListService {
 
-  ingredientsChanged = new EventEmitter<Ingredient[]>(); // emit our ingredients array
+  // ingredientsChanged = new EventEmitter<Ingredient[]>(); // emit our ingredients array
+  ingredientsChanged = new Subject<Ingredient>();
+
 
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
@@ -16,7 +18,9 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
-    this.ingredientsChanged.emit(this.ingredients.slice()); // pass here a copy of our ingredients array
+    //this.ingredientsChanged.emit(this.ingredients.slice()); // pass here a copy of our ingredients array
+    // @ts-ignore
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 
   addIngredients(ingredients: Ingredient[]) {
@@ -26,7 +30,9 @@ export class ShoppingListService {
     // });
 
     this.ingredients.push(...ingredients);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    // this.ingredientsChanged.emit(this.ingredients.slice());
+    // @ts-ignore
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 
 }
