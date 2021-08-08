a82800862bf748000c70383cbba0e7979d6908c4
NIDHAL BEN SABER <nidhal.ben-saber@talan.com>
2021-08-08 10:21:59 +0100

    init for form reactive example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import {Component, ViewChild} from '@angular/core';
-import {NgForm} from "@angular/forms";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,41 +6,5 @@ import {NgForm} from "@angular/forms";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  @ViewChild('f') signupForm!: NgForm;
-  defaultQuestion = 'teacher';
-  answer = '';
   genders = ['male', 'female'];
-  submitted = false;
-
-  user = {username: '', email: '', secretQuestion: '', answer: '', gender: ''};
-
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-    this.signupForm.form.patchValue({
-      userData: {
-        username: "Superuser"
-      }
-    });
-  }
-
-  // onSubmit(form: NgForm) {
-  //   console.log('submitted');
-  //   console.log(form);
-  // }
-
-  onSubmit() {
-    console.log(this.signupForm);
-
-    this.submitted = true;
-    this.user.username = this.signupForm.value.userData.username;
-    this.user.email = this.signupForm.value.userData.email;
-    this.user.secretQuestion = this.signupForm.value.secret;
-    this.user.answer = this.signupForm.value.questionAnswer;
-    this.user.gender = this.signupForm.value.gender;
-
-    this.signupForm.reset();
-
-
-  }
 }
