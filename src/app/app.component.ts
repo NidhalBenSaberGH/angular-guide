44f2d19b467f6e0875a33facfbe5606aae447c4c
NIDHAL BEN SABER <nidhal.ben-saber@talan.com>
2021-08-16 17:40:03 +0100

    usisng subject for error handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,32 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {map} from "rxjs/operators";
 import {Post} from "./post.model";
-import {PostsService} from "./PostsService";
+import {PostsService} from "./posts-service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
 
   loadedPosts: Post[] = [];
   isFetching = false;
   error = null;
 
+  private errorSubscription!: Subscription;
+
   constructor(private http: HttpClient, private postService: PostsService) {
   }
 
   ngOnInit() {
+    this.errorSubscription = this.postService.error.subscribe(errorMessage => {
+      // @ts-ignore
+      this.error = errorMessage;
+    });
     this.isFetching = true;
-    this.onFetchPosts();
   }
 
   onCreatePost(postData: Post) {
@@ -42,5 +48,9 @@ export class AppComponent implements OnInit {
     this.postService.deletePosts().subscribe(() => {
       this.loadedPosts = [];
     })
+  }
+
+  ngOnDestroy(): void {
+    this.errorSubscription.unsubscribe();
   }
 }
--- src/app/posts-service.ts
+++ src/app/posts-service.ts
@@ -2,10 +2,13 @@ import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {Post} from "./post.model";
 import {map} from "rxjs/operators";
+import {Subject} from "rxjs";
 
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
+
+  error = new Subject();
 
   constructor(private http: HttpClient) {
   }
@@ -18,6 +21,8 @@ export class PostsService {
       ).subscribe(
       responseData => {
         console.log(responseData);
+      }, error => {
+        this.error = error.message;
       });
   }
 
