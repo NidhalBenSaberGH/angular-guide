8cab4c8b977929065345f2485e3d30b87c5d1dae
NIDHAL BEN SABER <nidhal.ben-saber@talan.com>
2021-08-16 15:23:32 +0100

    firebase post and get with pipe map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
@@ -9,18 +10,41 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent implements OnInit {
   loadedPosts = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.onFetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
 
-    console.log(postData);
-
+    this.http
+      .post(
+        'https://angular-guide-17a98-default-rtdb.firebaseio.com/' + 'posts.json',
+        postData
+      )
+      .subscribe(responseData => {
+        console.log(responseData);
+      });
   }
 
-  onFetchPosts() {
-    // Send Http request
+  private onFetchPosts() {
+    this.http.get('https://angular-guide-17a98-default-rtdb.firebaseio.com/' + 'posts.json')
+      .pipe(map(responseData => {
+        const postsArray = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+            // @ts-ignore
+            postsArray.push({...responseData[key], id: key});
+          }
+        }
+        return postsArray;
+      }))
+      .subscribe(posts => {
+
+        console.log(posts);
+      });
   }
 
   onClearPosts() {
