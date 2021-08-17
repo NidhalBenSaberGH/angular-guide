8e8da7a92ea8f829188f7f09e5cbabb06dab4334
NIDHAL BEN SABER <nidhal.ben-saber@talan.com>
2021-08-17 13:05:05 +0100

    simple chnage

--- src/app/recipes/recipe-resolver.service.ts
+++ src/app/recipes/recipe-resolver.service.ts
@@ -3,15 +3,23 @@ import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from "@angular/rou
 import {Recipe} from "./recipe-list/recipe.model";
 import {Observable} from "rxjs";
 import {DataStorageService} from "../shared/data-storage.service";
+import {RecipeService} from "./recipe.service";
 
 @Injectable({providedIn: 'root'})
 export class RecipeResolverService implements Resolve<Recipe[]> {
 
-  constructor(private dataStorageService: DataStorageService) {
+  constructor(private dataStorageService: DataStorageService, private recipeService :RecipeService) {
   }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Recipe[]> | Promise<Recipe[]> | Recipe[] {
-    return this.dataStorageService.fetchRecipes(); // I not subscribe here but resolver will subscribe for me to find out once data is there.
+
+    const recipes = this.recipeService.getRecipes();
+    if (recipes.length === 0) {
+      return this.dataStorageService.fetchRecipes(); // I not subscribe here but resolver will subscribe for me to find out once data is there.
+    } else {
+      return  recipes;
+    }
+
   }
 
 }
