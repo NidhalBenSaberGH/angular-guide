5a051c306488ff9a1f9c4dc0f3ad862a89efe074
NIDHAL BEN SABER <nidhal.ben-saber@talan.com>
2021-08-18 17:19:04 +0100

    adding autologin save user in localStorage and set next user subject loaded user if exist

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -91,8 +91,29 @@ export class AuthService {
       expirationData
     );
 
+
+    localStorage.setItem('userData', JSON.stringify(user));
+
     this.user.next(user);
 
+  }
+
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: Date;
+    } = JSON.parse(<string>localStorage.getItem('userData'));
+    if (!userData) {
+      return;
+    }
+
+    const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
+
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+    }
   }
 
 }
