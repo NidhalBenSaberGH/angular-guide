52b8c0e68c40b3404e9d3a53ef4015e828bd8900
NIDHAL BEN SABER <nidhal.ben-saber@talan.com>
2021-08-23 17:04:23 +0100

    add shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,17 @@ import { HeaderComponent } from './header/header.component';
 import {BasicHighlightDirective} from "./shared/basic-highlight/basic-highlight.directive";
 import { BetterHighlightDirective } from './shared/better-highlight/better-highlight.directive';
 import { UnlessDirective } from './unless.directive';
-import { DropdownDirective } from './shared/dropdown.directive';
 import {ShoppingListService} from "./shopping-list/shopping-list.service";
 import {AppRoutingModule} from "./app-routing.module";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {RecipeService} from "./recipes/recipe.service";
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import { AuthComponent } from './auth/auth.component';
-import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import {AuthInterceptorService} from "./auth/auth-interceptor.service";
 import {AlertComponent} from "./shared/alert/alert.component";
-import {PlaceholderDirective} from "./shared/placeholder/placeholder.directive";
 import {RecipesModule} from "./recipes/recipes.module";
 import {ShoppingListModule} from "./shopping-list/shopping-list.module";
+import {SharedModule} from "./shared/shared.module";
 
 @NgModule({
   declarations: [
@@ -28,11 +26,7 @@ import {ShoppingListModule} from "./shopping-list/shopping-list.module";
     BasicHighlightDirective,
     BetterHighlightDirective,
     UnlessDirective,
-    DropdownDirective,
-    AuthComponent,
-    LoadingSpinnerComponent,
-    AlertComponent,
-    PlaceholderDirective
+    AuthComponent
   ],
   imports: [
     BrowserModule,
@@ -42,13 +36,14 @@ import {ShoppingListModule} from "./shopping-list/shopping-list.module";
     ReactiveFormsModule,
     HttpClientModule,
     RecipesModule,
-    ShoppingListModule
+    ShoppingListModule,
+    SharedModule
   ],
   providers: [ShoppingListService, RecipeService, {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 
-  entryComponents: [
-    AlertComponent
-  ]
+  // entryComponents: [
+  //   AlertComponent
+  // ]
 })
 export class AppModule { }
